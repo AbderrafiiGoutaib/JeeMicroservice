e503b6d12fcd22eef157985fafdb17abc1bbcee2
AbderrafiiGoutaib <63517761+AbderrafiiGoutaib@users.noreply.github.com>
2020-12-26 13:41:46 +0100

    uploadProject1

--- authentidication-service/authentidication-service/src/main/java/sid/authentidicationservice/security/repository/AppRoleRepository.java
+++
@@ -1,8 +0,0 @@
-package sid.authentidicationservice.security.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import sid.authentidicationservice.security.entities.AppRole;
-
-public interface AppRoleRepository extends JpaRepository<AppRole,Long> {
-    AppRole findAppRoleName(String roleName);
-}
--- authentidication-service/authentidication-service/src/main/java/sid/authentidicationservice/security/repository/AppUserRepository.java
+++
@@ -1,8 +0,0 @@
-package sid.authentidicationservice.security.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import sid.authentidicationservice.security.entities.AppUser;
-
-public interface AppUserRepository extends JpaRepository<AppUser,Long> {
-    AppUser findByUsername(String username);
-}
